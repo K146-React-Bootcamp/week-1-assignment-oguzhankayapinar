e56e035c9431f98a03177bf48d7604669571b5f2
Oguzhan Kayapinar <oguzhankayapinar@gmail.com>
2022-09-21 15:59:46 +0300

    ilk deneme

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Cookielerin tanımıyla başlayalım. 
+Cookie : Cookie (çerez), kullanıcının bilgisayarında web tarayıcılar aracılığıyla saklanan ve en fazla 4 KB boyutunda olan metin (txt) dosyalarıdır. 
+
+JavaScript ile cookie oluşturmak için document nesnesi kullanılır.
+
+Ödeve başlamadan önce deneme commiti gönderip pushlayalım
+*/
+
